7e126ea493aa5c8eef2f3a21c4113230b8060b86
Ekaterina-timans <128321173+Ekaterina-timans@users.noreply.github.com>
2024-08-06 13:50:53 +0300

    Added webpack.config.js and the weather__bar section

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                },
+            },
+        ]
+    },
+    plugins: [,
+        new Dotenv(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    mode: 'development'
+};
